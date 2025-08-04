4102b8308ec255136aceb66105eaf1658c3ce8ec
zeros <zeros.null@gmail.com>
2025-08-04 23:07:33 +0800

    修改名字，为别的使用case做准备

--- solve/run.sh
+++ solve/run.sh
@@ -1,5 +1,5 @@
-package_id="0x097a3833b6b5c62ca6ad10f0509dffdadff7ce31e1d86e63e884a14860cedc0f"
-challenge_object_id="0x19e76ca504c5a5fa5e214a45fca6c058171ba333f6da897b82731094504d5ab9"
+package_id=""
+challenge_object_id=""
 
 proof=0xd4dd5b0000000000
 github_id="0x-zeros"
