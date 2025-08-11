dfa8547eb1fcffb8d561d25cd1bf9e7b8aa5db50
zeros <zeros.null@gmail.com>
2025-08-11 18:43:43 +0800

    添加sui faucet 代码

--- solve/src/faucet.ts
+++ solve/src/faucet.ts
@@ -1,21 +1,29 @@
 import { getFaucetHost, requestSuiFromFaucetV2 } from '@mysten/sui/faucet';
 
-const address = '';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const address = process.env.ADDRESS || '';
+const network = (process.env.NETWORK as 'testnet' | 'devnet' | 'localnet') || 'testnet';
 
 //基本上没办法正常跑
 
 
 async function getTestSui() {
-  const faucetUrl = getFaucetHost('testnet'); //'https://faucet.testnet.sui.io/v2/gas'; // Testnet faucet 地址
+    
+  const faucetUrl = getFaucetHost(network); //'https://faucet.testnet.sui.io/v2/gas'; // Testnet faucet 地址
 
   //https://faucet.testnet.sui.io
-  console.log('faucetUrl', faucetUrl);
+  console.log(`${network} faucetUrl: ${faucetUrl}`);
 
   const response = await requestSuiFromFaucetV2({
     host: faucetUrl,
     recipient: address,
   });
-  console.log(response);
+  console.log(`recipient: ${address}`);
+  console.log('response', response);
 
 //FaucetRateLimitError: Too many requests from this client have been sent to the faucet. Please retry later
 
